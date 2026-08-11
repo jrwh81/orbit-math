78dde54274373baef4a06d8ccebd4fa79ebd423f
John Hansen <john.hansen@maryland.gov>
2026-08-11 02:41:46 -0400

    Fix production logger setup for Rails 7.1 compatibility

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.force_ssl = true
 
   config.log_tags = [:request_id]
-  config.logger = ActiveSupport::TaggedLogging.logger(STDOUT)
+  config.logger = ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new(STDOUT))
 
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
